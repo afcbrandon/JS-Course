2563c725b506b3e934d7789290994f51f0bd5e62
afcbrandon <35512660+afcbrandon@users.noreply.github.com>
2021-07-02 08:58:57 -0700

    update

---
+++ js/espacingstring.js
@@ -0,0 +1,1 @@
+document.write("We are the so-called \"Vikings\" from the north.");
---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.write("its lit ");
+var x = "variables bro"
+var k = "weed" + "for ever"
+var squad = "the gang", leader = "brandon", boss = "jersey";
+alert(x);
+var squad = squad.fontcolor("blue");
+document.write("We are the so-called \"Vikings\" from the north.   ");
+document.write("\"Be who you are and say what you feel,"
++ " because trhose who ind don\'t matter and those who matter don\'t mind."
++ "-dr. Suess ");
+document.write( squad )
+document.write( 5 + 5);
