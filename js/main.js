079932fc217c7703b3ffee2788c680507b52fd4b
afcbrandon <35512660+afcbrandon@users.noreply.github.com>
2021-07-02 15:05:20 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-document.write("its lit ");
-var x = "variables bro"
-var k = "weed" + "for ever"
-var squad = "the gang", leader = "brandon", boss = "jersey";
-alert(x);
-var squad = squad.fontcolor("blue");
-document.write("We are the so-called \"Vikings\" from the north.   ");
-document.write("\"Be who you are and say what you feel,"
-+ " because trhose who ind don\'t matter and those who matter don\'t mind."
-+ "-dr. Suess ");
-document.write( squad )
-document.write( 5 + 5);
+function afunction() {
+    var str = "awh shucks you clicked it";
+    var result = str.fontcolor("red");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+window.alert("this is a window alert");
+
+var A = "this is variable A ";
+var B = "this is variable B  ";
+result = B.fontcolor("blue");
+
+document.write(A + result ); // writing to the document while combing a variable and variable with font color
+
+
