45f9c35102b1d83ecea776f7faa2b2da857c7201
afcbrandon <35512660+afcbrandon@users.noreply.github.com>
2021-07-07 13:12:49 -0700

    myb

---
+++ basic_javascript_projects/NaN/js/main.js
@@ -0,0 +1,10 @@
+function myfunction() {
+    document.getElementById("para").innerHTML = 0/0;
+}
+function nanstring() {
+    document.getElementById("string").innerHTML = isNaN("this is a string");
+}
+function nannumber() {
+    document.getElementById("number").innerHTML = isNaN("420");
+
+}
